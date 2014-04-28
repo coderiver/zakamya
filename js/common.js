33c4940bcb4523723a953cb17199443949f94689
hraboviyvadim <hraboviyvadim@gmail.com>
2014-04-28 18:01:30 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,49 @@
 $(document).ready(function() {
-	console.log($('body').html());
+	
+	// select
+    function select() {
+        $(".js-select").each(function(){
+            var select_list = $(this).find(".js-select-list");
+            var text = select_list.find("li").first().text();
+            select_list.hide();
+            $(this).find(".js-select-text").text(text);
+            $(this).click(function(event){
+                if ($(this).hasClass("is-active")) {
+                    $(this).removeClass("is-active");
+                    select_list.slideUp("fast");
+                }
+                else {
+                    $(".js-select").removeClass("is-active");
+                    $(".js-select-list").hide();
+                    select_list.slideDown("fast");
+                    $(this).addClass("is-active");
+                }
+                event.stopPropagation();
+            });
+            select_list.find("li").click(function(event) {
+                var id = $(this).attr("data-id");
+                var text = $(this).text();
+                $(this).parent().parent().find(".js-select-text").text(text);
+                $(this).parent().parent().find(".js-select-input").val(id);
+                $(this).parent().hide();
+                $(this).parents(".js-select").removeClass("is-active");
+                event.stopPropagation();
+                return false;
+            });
+            $(".help").on('click', function(event){
+                $(".js-select-list").hide();
+                event.stopPropagation();
+            });
+        });
+
+    }
+    select();
+    $('.js-select').click(function(event){
+        event.stopPropagation();
+    });
+
+    $(document).click(function(event){
+    	$(".js-select-list").slideUp('fast');
+	});
+
 });
