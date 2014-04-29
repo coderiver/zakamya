f48831702cb5f630b789a1ad0d7e29e737d5f9ae
hraboviyvadim <hraboviyvadim@gmail.com>
2014-04-29 17:50:40 +0300

    index content

--- js/common.js
+++ js/common.js
@@ -65,6 +65,30 @@ $(document).ready(function() {
     }	
     menu();
 
+    // tabs
+
+    function tab() {
+       $(".js-tab").each(function(){
+         var tab_link = $(this).find("a");
+         var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+         tab_cont.hide();
+         $(this).parents(".js-tab-group").find(".js-tab1").show();
+         $(this).find('li:nth-child(1)').addClass('is-active');
+            tab_link.on("click", function() {
+                var index = $(this).attr("href");
+                tab_link.removeClass("is-active");
+                tab_link.parent().removeClass("is-active");
+                $(this).addClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parents(".js-tab-group").find("."+index).show();
+                return false;
+            });
+        });
+    }
+    if ($(".js-tab-group").length) {
+        tab();
+    };
 
     $(document).click(function(event){
     	$(".js-select-list").slideUp('fast');
